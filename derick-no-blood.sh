7d8a2c276272aa1d22f7ac8ffa18de413546fd21
derick-webnettips-com <derick.nyatsanza@webnettips.com>
2020-12-25 09:52:44 +0000

    first commit

---
+++ derick-no-blood.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+cat<<EOF > ${HOME}/derick-no-blood.txt
+Gen 9:4 
+ 4 Only flesh with its life*—its blood+—you must not eat
+
+Leviticus 17:10
+10  “‘If any man of the house of Israel or any foreigner who is residing in your midst eats any sort of blood,a I will certainly set my face against the one* who is eating the blood, and I will cut him off* from among his people.
+
+Deuteronomy 12:23
+23  Just be firmly resolved not to eat the blood,a because the blood is the life,*b and you must not eat the life* with the flesh.
+
+Acts 15:28, 29
+28  For the holy spirita and we ourselves have favored adding no further burden to you except these necessary things: 29  to keep abstaining from things sacrificed to idols,b from blood,c from what is strangled,d and from sexual immorality.e If you carefully keep yourselves from these things, you will prosper. Good health to you!”
+
+EOF
+
+cat ${HOME}/derick-no-blood.txt
+
+rm -f ${HOME}/derick-no-blood.txt
+
+
+
+
